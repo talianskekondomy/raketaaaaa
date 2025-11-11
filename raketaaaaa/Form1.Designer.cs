8d9eb9e5c197beaf8e97953b576d252f1df94a00
talianskekondomy <monniebush@gmail.com>
2025-11-11 13:09:55 +0100

    Upravená logika pohybu rakety a slider rychlosti, přidány komentáře

--- raketaaaaa/Form1.Designer.cs
+++ raketaaaaa/Form1.Designer.cs
@@ -1,132 +1,76 @@
-﻿namespace raketaaaaa
+﻿using System.Windows.Forms;
+
+namespace raketaaaaa
 {
     partial class Form1
     {
-        /// <summary>
-        /// Vyžaduje se proměnná návrháře.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Uvolněte všechny používané prostředky.
-        /// </summary>
-        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
             base.Dispose(disposing);
         }
 
-        #region Kód generovaný Návrhářem Windows Form
+        #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
-        /// obsah této metody v editoru kódu.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtNadpisRaketa = new System.Windows.Forms.Label();
             this.hraciPlocha = new System.Windows.Forms.GroupBox();
+            this.boxMeteorit = new System.Windows.Forms.PictureBox();
+            this.boxRaketa = new System.Windows.Forms.PictureBox();
             this.txtScore = new System.Windows.Forms.Label();
             this.txtZivoty = new System.Windows.Forms.Label();
             this.txtPalivo = new System.Windows.Forms.Label();
             this.BoxSkore = new System.Windows.Forms.TextBox();
             this.BoxPalivo = new System.Windows.Forms.TextBox();
             this.boxZivot1 = new System.Windows.Forms.PictureBox();
-            this.boxMeteorit = new System.Windows.Forms.PictureBox();
-            this.boxRaketa = new System.Windows.Forms.PictureBox();
             this.boxZivot2 = new System.Windows.Forms.PictureBox();
-            this.boxzivot3 = new System.Windows.Forms.PictureBox();
+            this.boxZivot3 = new System.Windows.Forms.PictureBox();
+
+            // === OVLÁDACÍ PANEL ===
+            this.groupOvládání = new System.Windows.Forms.GroupBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.trackBarRychlost = new System.Windows.Forms.TrackBar();
+            this.progressBarPalivo = new System.Windows.Forms.ProgressBar();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelRychlost = new System.Windows.Forms.Label();
+
             this.hraciPlocha.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.boxZivot1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxMeteorit)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxRaketa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxZivot1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxZivot2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.boxzivot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxZivot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRychlost)).BeginInit();
+            this.groupOvládání.SuspendLayout();
             this.SuspendLayout();
-            // 
+
             // txtNadpisRaketa
-            // 
             this.txtNadpisRaketa.AutoSize = true;
-            this.txtNadpisRaketa.Location = new System.Drawing.Point(491, 17);
+            this.txtNadpisRaketa.Location = new System.Drawing.Point(490, 15);
             this.txtNadpisRaketa.Name = "txtNadpisRaketa";
             this.txtNadpisRaketa.Size = new System.Drawing.Size(111, 22);
             this.txtNadpisRaketa.TabIndex = 0;
             this.txtNadpisRaketa.Text = "Hra Raketa";
             this.txtNadpisRaketa.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
+
             // hraciPlocha
-            // 
             this.hraciPlocha.BackColor = System.Drawing.SystemColors.Desktop;
             this.hraciPlocha.Controls.Add(this.boxMeteorit);
             this.hraciPlocha.Controls.Add(this.boxRaketa);
-            this.hraciPlocha.Location = new System.Drawing.Point(-5, 51);
+            this.hraciPlocha.Location = new System.Drawing.Point(-5, 50);
             this.hraciPlocha.Name = "hraciPlocha";
             this.hraciPlocha.Size = new System.Drawing.Size(1107, 523);
             this.hraciPlocha.TabIndex = 1;
             this.hraciPlocha.TabStop = false;
-            this.hraciPlocha.Enter += new System.EventHandler(this.hraciPlocha_Enter);
-            // 
-            // txtScore
-            // 
-            this.txtScore.AutoSize = true;
-            this.txtScore.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtScore.Location = new System.Drawing.Point(21, 588);
-            this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(68, 22);
-            this.txtScore.TabIndex = 0;
-            this.txtScore.Text = "Skóre:";
-            // 
-            // txtZivoty
-            // 
-            this.txtZivoty.AutoSize = true;
-            this.txtZivoty.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtZivoty.Location = new System.Drawing.Point(510, 588);
-            this.txtZivoty.Name = "txtZivoty";
-            this.txtZivoty.Size = new System.Drawing.Size(70, 22);
-            this.txtZivoty.TabIndex = 2;
-            this.txtZivoty.Text = "Životy:";
-            // 
-            // txtPalivo
-            // 
-            this.txtPalivo.AutoSize = true;
-            this.txtPalivo.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.txtPalivo.Location = new System.Drawing.Point(923, 588);
-            this.txtPalivo.Name = "txtPalivo";
-            this.txtPalivo.Size = new System.Drawing.Size(71, 22);
-            this.txtPalivo.TabIndex = 3;
-            this.txtPalivo.Text = "Palivo:";
-            // 
-            // BoxSkore
-            // 
-            this.BoxSkore.Location = new System.Drawing.Point(95, 583);
-            this.BoxSkore.Name = "BoxSkore";
-            this.BoxSkore.Size = new System.Drawing.Size(61, 28);
-            this.BoxSkore.TabIndex = 4;
-            // 
-            // BoxPalivo
-            // 
-            this.BoxPalivo.Location = new System.Drawing.Point(1000, 584);
-            this.BoxPalivo.Name = "BoxPalivo";
-            this.BoxPalivo.Size = new System.Drawing.Size(62, 28);
-            this.BoxPalivo.TabIndex = 5;
-            // 
-            // boxZivot1
-            // 
-            this.boxZivot1.Image = global::raketaaaaa.Properties.Resources.valentine_heart_icon_6;
-            this.boxZivot1.Location = new System.Drawing.Point(586, 588);
-            this.boxZivot1.Name = "boxZivot1";
-            this.boxZivot1.Size = new System.Drawing.Size(26, 25);
-            this.boxZivot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.boxZivot1.TabIndex = 6;
-            this.boxZivot1.TabStop = false;
-            // 
+
             // boxMeteorit
-            // 
             this.boxMeteorit.Image = global::raketaaaaa.Properties.Resources.Meteorit;
             this.boxMeteorit.Location = new System.Drawing.Point(48, 27);
             this.boxMeteorit.Name = "boxMeteorit";
@@ -134,44 +78,145 @@
             this.boxMeteorit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.boxMeteorit.TabIndex = 1;
             this.boxMeteorit.TabStop = false;
-            // 
-            // boxRaketa
-            // 
+
+            // boxRaketa 
             this.boxRaketa.Image = global::raketaaaaa.Properties.Resources.raketa_png_vektorovy_obrazek_ilustra_ni_klipart_raketa_zdarma_ke_sta_eni_doprava_vektor_do_vasich_dokument_286;
-            this.boxRaketa.InitialImage = ((System.Drawing.Image)(resources.GetObject("boxRaketa.InitialImage")));
             this.boxRaketa.Location = new System.Drawing.Point(528, 433);
             this.boxRaketa.Name = "boxRaketa";
             this.boxRaketa.Size = new System.Drawing.Size(57, 63);
             this.boxRaketa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.boxRaketa.TabIndex = 0;
             this.boxRaketa.TabStop = false;
-            // 
+
+            // txtScore
+            this.txtScore.AutoSize = true;
+            this.txtScore.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtScore.Location = new System.Drawing.Point(20, 585);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(68, 22);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Skóre:";
+
+            // txtZivoty
+            this.txtZivoty.AutoSize = true;
+            this.txtZivoty.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtZivoty.Location = new System.Drawing.Point(510, 585);
+            this.txtZivoty.Name = "txtZivoty";
+            this.txtZivoty.Size = new System.Drawing.Size(70, 22);
+            this.txtZivoty.TabIndex = 3;
+            this.txtZivoty.Text = "Životy:";
+
+            // txtPalivo
+            this.txtPalivo.AutoSize = true;
+            this.txtPalivo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtPalivo.Location = new System.Drawing.Point(923, 585);
+            this.txtPalivo.Name = "txtPalivo";
+            this.txtPalivo.Size = new System.Drawing.Size(71, 22);
+            this.txtPalivo.TabIndex = 4;
+            this.txtPalivo.Text = "Palivo:";
+
+            // BoxSkore
+            this.BoxSkore.Location = new System.Drawing.Point(90, 580);
+            this.BoxSkore.Name = "BoxSkore";
+            this.BoxSkore.Size = new System.Drawing.Size(60, 28);
+            this.BoxSkore.TabIndex = 5;
+
+            // BoxPalivo
+            this.BoxPalivo.Location = new System.Drawing.Point(1000, 580);
+            this.BoxPalivo.Name = "BoxPalivo";
+            this.BoxPalivo.Size = new System.Drawing.Size(60, 28);
+            this.BoxPalivo.TabIndex = 6;
+
+            // boxZivot1
+            this.boxZivot1.Image = global::raketaaaaa.Properties.Resources.valentine_heart_icon_6;
+            this.boxZivot1.Location = new System.Drawing.Point(585, 585);
+            this.boxZivot1.Name = "boxZivot1";
+            this.boxZivot1.Size = new System.Drawing.Size(26, 25);
+            this.boxZivot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boxZivot1.TabIndex = 7;
+            this.boxZivot1.TabStop = false;
+
             // boxZivot2
-            // 
             this.boxZivot2.Image = global::raketaaaaa.Properties.Resources.valentine_heart_icon_6;
-            this.boxZivot2.Location = new System.Drawing.Point(618, 588);
+            this.boxZivot2.Location = new System.Drawing.Point(617, 585);
             this.boxZivot2.Name = "boxZivot2";
             this.boxZivot2.Size = new System.Drawing.Size(26, 25);
             this.boxZivot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.boxZivot2.TabIndex = 7;
+            this.boxZivot2.TabIndex = 8;
             this.boxZivot2.TabStop = false;
-            // 
-            // boxzivot3
-            // 
-            this.boxzivot3.Image = global::raketaaaaa.Properties.Resources.valentine_heart_icon_6;
-            this.boxzivot3.Location = new System.Drawing.Point(650, 588);
-            this.boxzivot3.Name = "boxzivot3";
-            this.boxzivot3.Size = new System.Drawing.Size(26, 25);
-            this.boxzivot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.boxzivot3.TabIndex = 8;
-            this.boxzivot3.TabStop = false;
-            // 
+
+           // boxZivot3
+            this.boxZivot3.Image = global::raketaaaaa.Properties.Resources.valentine_heart_icon_6;
+            this.boxZivot3.Location = new System.Drawing.Point(649, 585);
+            this.boxZivot3.Name = "boxZivot3";
+            this.boxZivot3.Size = new System.Drawing.Size(26, 25);
+            this.boxZivot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.boxZivot3.TabIndex = 9;
+            this.boxZivot3.TabStop = false;
+
+            // groupOvládání
+            this.groupOvládání.Controls.Add(this.buttonStart);
+            this.groupOvládání.Controls.Add(this.buttonStop);
+            this.groupOvládání.Controls.Add(this.buttonReset);
+            this.groupOvládání.Controls.Add(this.trackBarRychlost);
+            this.groupOvládání.Controls.Add(this.progressBarPalivo);
+            this.groupOvládání.Controls.Add(this.labelInfo);
+            this.groupOvládání.Controls.Add(this.labelRychlost);
+            this.groupOvládání.Location = new System.Drawing.Point(12, 625);
+            this.groupOvládání.Name = "groupOvládání";
+            this.groupOvládání.Size = new System.Drawing.Size(1070, 120);
+            this.groupOvládání.TabIndex = 10;
+            this.groupOvládání.TabStop = false;
+            this.groupOvládání.Text = "Ovládání";
+ 
+            // buttonStart
+            this.buttonStart.Text = "Start";
+            this.buttonStart.Location = new System.Drawing.Point(20, 30);
+            this.buttonStart.Size = new System.Drawing.Size(80, 30);
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+
+            // buttonStop
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.Location = new System.Drawing.Point(110, 30);
+            this.buttonStop.Size = new System.Drawing.Size(80, 30);
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+
+            // buttonReset
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.Location = new System.Drawing.Point(200, 30);
+            this.buttonReset.Size = new System.Drawing.Size(80, 30);
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+
+            // trackBarRychlost
+            this.trackBarRychlost.Location = new System.Drawing.Point(320, 30);
+            this.trackBarRychlost.Maximum = 20;
+            this.trackBarRychlost.Minimum = 5;
+            this.trackBarRychlost.Value = 10;
+            this.trackBarRychlost.TickFrequency = 1;
+            this.trackBarRychlost.Size = new System.Drawing.Size(200, 45);
+            this.trackBarRychlost.Scroll += new System.EventHandler(this.trackBarRychlost_Scroll);
+            trackBarRychlost.TabStop = false;
+
+            // labelRychlost
+            this.labelRychlost.Text = "Rychlost: 10";
+            this.labelRychlost.Location = new System.Drawing.Point(530, 35);
+
+            // progressBarPalivo
+            this.progressBarPalivo.Location = new System.Drawing.Point(650, 35);
+            this.progressBarPalivo.Maximum = 100;
+            this.progressBarPalivo.Size = new System.Drawing.Size(200, 25);
+ 
+            // labelInfo
+            this.labelInfo.Location = new System.Drawing.Point(870, 35);
+            this.labelInfo.Size = new System.Drawing.Size(180, 40);
+            this.labelInfo.Text = "Skóre: 0 | Životy: 3 | Palivo: 100%";
+
             // Form1
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 619);
-            this.Controls.Add(this.boxzivot3);
+            this.ClientSize = new System.Drawing.Size(1100, 760);
+            this.Controls.Add(this.groupOvládání);
+            this.Controls.Add(this.boxZivot3);
             this.Controls.Add(this.boxZivot2);
             this.Controls.Add(this.boxZivot1);
             this.Controls.Add(this.BoxPalivo);
@@ -183,19 +228,20 @@
             this.Controls.Add(this.txtNadpisRaketa);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Hra raketa";
             this.Load += new System.EventHandler(this.Form1_Load);
+
             this.hraciPlocha.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.boxZivot1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxMeteorit)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxRaketa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxZivot1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.boxZivot2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.boxzivot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxZivot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRychlost)).EndInit();
+            this.groupOvládání.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
@@ -211,7 +257,16 @@
         private System.Windows.Forms.PictureBox boxMeteorit;
         private System.Windows.Forms.PictureBox boxZivot1;
         private System.Windows.Forms.PictureBox boxZivot2;
-        private System.Windows.Forms.PictureBox boxzivot3;
+        private System.Windows.Forms.PictureBox boxZivot3;
+
+        // nové ovládací prvky
+        private System.Windows.Forms.GroupBox groupOvládání;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.TrackBar trackBarRychlost;
+        private System.Windows.Forms.ProgressBar progressBarPalivo;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelRychlost;
     }
 }
-
--- raketaaaaa/Form1.cs
+++ raketaaaaa/Form1.cs
@@ -6,56 +6,64 @@ namespace raketaaaaa
 {
     public partial class Form1 : Form
     {
-        int raketaRychlost = 10;
-        int meteoritRychlost = 10;
-        int skore = 0;
-        int zivoty = 3;
-        int palivo = 100;
+        // --- ZÁKLADNÍ PROMĚNNÉ ---
+        int raketaRychlost = 10;   // rychlost pohybu rakety
+        int meteoritRychlost = 10; // rychlost meteoritu
+        int skore = 0;             // skóre hráče
+        int zivoty = 3;            // počet životů
+        int palivo = 100;          // množství paliva
 
-        Random rnd = new Random();
+        Random rnd = new Random(); // náhodné pozice meteorů
+        bool pohybVlevo = false;   // jestli se raketa právě pohybuje vlevo
+        bool pohybVpravo = false;  // jestli se raketa právě pohybuje vpravo
 
-        bool pohybVlevo = false;
-        bool pohybVpravo = false;
-
-        PictureBox[] meteory;
-        PictureBox[] srdce;
-        Timer Casovac = new Timer();
+        PictureBox[] meteory;      // pole pro meteory
+        PictureBox[] srdce;        // pole pro ikonky životů
+        Timer Casovac = new Timer(); // hlavní časovač hry
 
         public Form1()
         {
             InitializeComponent();
 
-            // Zachycení kláves
+            // Umožní zachytit klávesy i když má fokus jiný prvek (např. tlačítko)
             this.KeyPreview = true;
+            this.ActiveControl = null; // zruší fokus z trackbaru, aby šipky hned fungovaly
+
+            // Události pro klávesy
             this.KeyDown += Form1_KeyDown;
             this.KeyUp += Form1_KeyUp;
 
-            // Inicializace rakety
+            // Umístím raketu na střed hrací plochy
             boxRaketa.Left = hraciPlocha.Width / 2 - boxRaketa.Width / 2;
             boxRaketa.Top = hraciPlocha.Height - boxRaketa.Height - 10;
 
-            // Inicializace meteoritu
-            PictureBox meteor1 = boxMeteorit; // z designeru
-            PictureBox meteor2 = new PictureBox(); // druhý meteorit
+            // --- METEORITY ---
+            // první meteorit je z Designeru
+            PictureBox meteor1 = boxMeteorit;
+            // druhý vytvořím dynamicky
+            PictureBox meteor2 = new PictureBox();
             meteor2.Size = meteor1.Size;
-            meteor2.SizeMode = PictureBoxSizeMode.Zoom;
+            meteor2.SizeMode = PictureBoxSizeMode.StretchImage;
             meteor2.Image = meteor1.Image;
             hraciPlocha.Controls.Add(meteor2);
 
+            // uložím meteory do pole a nastavím jejich pozici
             meteory = new PictureBox[] { meteor1, meteor2 };
+            foreach (var m in meteory)
+                ResetMeteor(m);
 
-            foreach (var meteor in meteory)
-                ResetMeteor(meteor);
+            // --- SRDCE (ŽIVOTY) ---
+            srdce = new PictureBox[] { boxZivot1, boxZivot2, boxZivot3 };
 
-            // Inicializace srdcí (všimni si, že poslední se jmenuje boxzivot3 s malým z)
-            srdce = new PictureBox[] { boxZivot1, boxZivot2, boxzivot3 };
+            // --- ČASOVAČ ---
+            Casovac.Interval = 50; // obnovování hry každých 50 ms
+            Casovac.Tick += GameLoop; // hlavní herní smyčka
 
-            // Timer
-            Casovac.Interval = 50;
-            Casovac.Tick += GameLoop;
-            Casovac.Start();
+            // Aktualizuju prvky hned po spuštění
+            AktualizujUI();
         }
 
+        // --- OVLÁDÁNÍ KLÁVESAMI ---
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Left) pohybVlevo = true;
@@ -68,9 +76,10 @@ namespace raketaaaaa
             if (e.KeyCode == Keys.Right) pohybVpravo = false;
         }
 
+        // --- HLAVNÍ SMYČKA HRY ---
         private void GameLoop(object sender, EventArgs e)
         {
-            // Pohyb rakety
+            // Pohyb rakety – jen pokud máme palivo
             if (pohybVlevo && boxRaketa.Left > 0 && palivo > 0)
             {
                 boxRaketa.Left -= raketaRychlost;
@@ -82,41 +91,41 @@ namespace raketaaaaa
                 palivo--;
             }
 
-            palivo = Math.Max(palivo, 0);
+            palivo = Math.Max(palivo, 0); // aby palivo nešlo do mínusu
 
             // Pohyb meteorů
-            for (int i = 0; i < meteory.Length; i++)
-                MoveMeteor(meteory[i]);
+            foreach (var meteor in meteory)
+                MoveMeteor(meteor);
 
-            // Aktualizace srdcí
+            // Aktualizace viditelných srdcí podle životů
             for (int i = 0; i < srdce.Length; i++)
                 srdce[i].Visible = (i < zivoty);
 
-            // Aktualizace UI
-            BoxSkore.Text = skore.ToString();
-            BoxPalivo.Text = palivo.ToString() + "%";
+            AktualizujUI();
         }
 
+        // --- POHYB METEORITU ---
         private void MoveMeteor(PictureBox meteor)
         {
             meteor.Top += meteoritRychlost;
 
-            // Kolize s raketou
+            // Kolize s raketou → ztráta života
             if (meteor.Bounds.IntersectsWith(boxRaketa.Bounds))
             {
                 zivoty--;
-                palivo = 100;
+                palivo = 100; // doplním palivo po zásahu
                 ResetMeteor(meteor);
 
                 if (zivoty <= 0)
                 {
                     Casovac.Stop();
-                    MessageBox.Show("Konec hry! Tvé skóre: " + skore);
-                    this.Close();
+                    hraciPlocha.BackColor = Color.DarkRed;
+                    MessageBox.Show($"Konec hry!\nTvé skóre: {skore}", "Game Over");
+                    return;
                 }
             }
 
-            // Meteor spadl mimo obrazovku
+            // Pokud meteorit spadne mimo okno -> přičti skóre
             if (meteor.Top > hraciPlocha.Height)
             {
                 skore++;
@@ -124,19 +133,68 @@ namespace raketaaaaa
             }
         }
 
+        // --- VRÁCENÍ METEORITU NAHORU ---
         private void ResetMeteor(PictureBox meteor)
         {
             meteor.Left = rnd.Next(0, hraciPlocha.Width - meteor.Width);
             meteor.Top = -meteor.Height;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        // --- AKTUALIZACE TEXTŮ A PRVKŮ ---
+        private void AktualizujUI()
         {
-            // sem můžeš doplnit logiku po načtení formuláře
+            BoxSkore.Text = skore.ToString();
+            BoxPalivo.Text = $"{palivo}%";
+
+            progressBarPalivo.Value = Math.Max(0, Math.Min(palivo, 100));
+            labelInfo.Text = $"Skóre: {skore} | Životy: {zivoty} | Palivo: {palivo}%";
+            labelRychlost.Text = $"Rychlost: {raketaRychlost}";
         }
 
-        private void hraciPlocha_Enter(object sender, EventArgs e)
+        // --- TLAČÍTKA ---
+        private void buttonStart_Click(object sender, EventArgs e)
         {
+            Casovac.Start();
+            hraciPlocha.BackColor = Color.Black;
         }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            Casovac.Stop();
+            hraciPlocha.BackColor = Color.DarkSlateGray;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            Casovac.Stop();
+            skore = 0;
+            zivoty = 3;
+            palivo = 100;
+            raketaRychlost = 10;
+            meteoritRychlost = 10;
+
+            foreach (var m in meteory)
+                ResetMeteor(m);
+
+            // raketu vrátím na střed
+            boxRaketa.Left = hraciPlocha.Width / 2 - boxRaketa.Width / 2;
+
+            AktualizujUI();
+            hraciPlocha.BackColor = Color.Black;
+        }
+
+        // --- SLIDER NA RYCHLOST ---
+        private void trackBarRychlost_Scroll(object sender, EventArgs e)
+        {
+            raketaRychlost = trackBarRychlost.Value;
+            labelRychlost.Text = $"Rychlost: {raketaRychlost}";
+
+            // Po posunu slideru zruším fokus → šipky zase ovládají raketu
+            this.ActiveControl = null;
+        }
+
+        // --- OSTATNÍ METODY (nepoužité, ale nechávám pro přehled) ---
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void hraciPlocha_Enter(object sender, EventArgs e) { }
     }
 }
